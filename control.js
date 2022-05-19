c11c8dd7c456211047d7d56bd0a4bfb0cb96a536
devWaz <fawazodessy@gmail.com>
2022-05-19 17:12:07 +0100

    commit

--- control.js
+++ control.js
@@ -1,3 +1,26 @@
-var currentTime = Date()
+function showTime(){
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+    var s = currentTime.getSeconds();
+    var session = 'AM';
 
-var h = currentTime.getHours()
+    if(h == 0){
+        h = 12;
+    }
+    if(h > 12){
+        h = h -12;
+        session = 'PM';
+    }
+
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    
+    var time = h + ':' + m + ':' + s + ' ' + session;
+    document.getElementById("time").innerText = time;
+
+}
+
+
+setInterval(showTime , 1000);
